4b8c2df1108c911ce88147718036b08721946af4
Mücahit Yıldırım <mucahityildirim@Mucahit-MacBook-Air.local>
2022-07-17 23:16:08 +0300

    create single post component

---
+++ src/pages/single/Single.jsx
@@ -0,0 +1,14 @@
+import "./single.css";
+import SideBar from "../../components/sidebar/SideBar";
+import SinglePost from "../../components/singlePosts/SinglePost";
+
+const Single = () => {
+  return (
+    <div className="single">
+      <SinglePost />
+      <SideBar />
+    </div>
+  );
+};
+
+export default Single;
