d32b2dfe03454262332a89f3b3af27f479c95501
Mücahit Yıldırım <mucahityildirim@Mucahit-MacBook-Air.local>
2022-07-17 15:16:59 +0300

    add header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import TopBar from "./topbar/TopBar";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <TopBar />
-    </div>
+      <Home />
+    </>
   );
 }
 
